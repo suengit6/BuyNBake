2f935259185e8eb3d6534c25df42a17a027a9a32
CatMeowlet <jamesvincent.ocariza@gmail.com>
2023-08-08 04:39:09 +0800

    change: add dynamic js and product

--- js/handle-dummy-data.js
+++ js/handle-dummy-data.js
@@ -1,6 +1,8 @@
+// The productsData function is an Immediately Invoked Function Expression (IIFE).
 const productsData = function () {
-    let $productsGallery;
+    let $productsGallery; // Variable to store the reference to the products gallery element
 
+    // Array containing product information
     const PRODUCT_LIST = [
         {
             name: 'Product 1',
@@ -22,34 +24,40 @@ const productsData = function () {
         }
     ];
 
+    // Function to initialize local storage with product data
     const handleInitLocalStoreProduct = () => {
+        // Logging the PRODUCT_LIST for reference
         console.log(JSON.parse(JSON.stringify(PRODUCT_LIST)));
 
+        // Check if 'products' key does not exist in the local storage
         if (!localStorage.getItem('products')) {
+            // If 'products' key doesn't exist, store the PRODUCT_LIST data as a JSON string
             localStorage.setItem('products', JSON.stringify(PRODUCT_LIST));
         } else {
+            // If 'products' key already exists, remove it and then store the PRODUCT_LIST data again
             localStorage.removeItem('products');
             localStorage.setItem('products', JSON.stringify(PRODUCT_LIST));
         }
 
-        // check if products not empty
+        // Retrieve data from local storage and check if it has at least one product
         let data = JSON.parse(localStorage.getItem('products'));
 
-        // Check if data is not null and has at least one item
         if (data && data.length > 0) {
-            console.log(data[0]);
+            console.log(data[0]); // Log the first product's information from local storage
         } else {
             console.log('No products in localStorage.');
         }
     }
 
-
+    // Function to dynamically render product information on the webpage
     const handleRenderProduct = () => {
         $productsGallery = document.querySelector('#products-gallery');
 
         let listHtml = [];
 
+        // Loop through each product in the PRODUCT_LIST array
         PRODUCT_LIST.forEach((val, index) => {
+            // Generate the HTML for each product item
             let containerHtml = `
             <li class="col-md-4">
                 <a href="product.html?id=${index}" target="_blank" >
@@ -69,23 +77,24 @@ const productsData = function () {
                 </a>
             </li>`;
 
+            // Add the generated HTML for this product to the listHtml array
             listHtml.push(containerHtml);
         });
 
+        // Join all the product HTML elements together and set them as the content of the productsGallery element
         $productsGallery.innerHTML = listHtml.join(' ');
     }
 
-
+    // Public API: Expose the init method to initialize the product data and render it on the webpage
     return {
         init: function () {
             handleInitLocalStoreProduct();
             handleRenderProduct();
         }
     }
-    
 }();
 
-
+// jQuery code to run the productsData.init() function after the document has been fully loaded
 $(document).ready(function () {
     productsData.init();
 });
